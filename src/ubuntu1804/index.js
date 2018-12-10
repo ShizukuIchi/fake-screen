b50062ad11d674760b157a859aa3cac7ace9a61e
ShizukuIchi <shizukuichi@gmail.com>
2018-12-10 16:31:39 +0800

    ubuntu mousedown listener

--- src/ubuntu1804/index.js
+++ src/ubuntu1804/index.js
@@ -1,5 +1,5 @@
-import _ubuntu from "./ubuntu1804.pug";
-import "./ubuntu1804.scss";
+import _ubuntu from './ubuntu1804.pug';
+import './ubuntu1804.scss';
 
 export const render = () => {
   setTimeout(start);
@@ -7,31 +7,34 @@ export const render = () => {
 };
 
 function start() {
-  const wrapper = document.querySelector(".ubuntu1804-wrapper");
+  const wrapper = document.querySelector('.ubuntu1804-wrapper');
 
-  const password = wrapper.querySelector("#password");
-  const signIn = wrapper.querySelector("#sign-in");
-  const cancel = wrapper.querySelector("#cancel");
-  const hint = wrapper.querySelector(".hint");
-  password.addEventListener("keydown", submit);
-  signIn.addEventListener("click", submit);
-  cancel.addEventListener("click", clear);
+  const password = wrapper.querySelector('#password');
+  const signIn = wrapper.querySelector('#sign-in');
+  const cancel = wrapper.querySelector('#cancel');
+  const hint = wrapper.querySelector('.hint');
+  const timeModal = wrapper.querySelector('.ubuntu1804-time-modal');
+  const loginScreen = wrapper.querySelector('.ubuntu1804-login-container');
+
+  password.addEventListener('keydown', submit);
+  signIn.addEventListener('click', submit);
+  cancel.addEventListener('click', clear);
   function submit({ key }) {
-    if (key && key !== "Enter") return;
-    hint.style.animation = "";
+    if (key && key !== 'Enter') return;
+    hint.style.animation = '';
     // a magic for replaying animation
     void hint.offsetWidth;
     hint.textContent = password.value.length
-      ? "Wrong password."
-      : "You must enter a password!";
-    hint.style.animation = "timeout 1.6s";
+      ? 'Wrong password.'
+      : 'You must enter a password!';
+    hint.style.animation = 'timeout 1.8s';
   }
   function clear() {
-    password.value = "";
+    password.value = '';
   }
-  password.addEventListener("mousemove", setIdleTimer);
-  document.addEventListener("keydown", setIdleTimer);
-  const timeModal = wrapper.querySelector(".ubuntu1804-time-modal");
+  password.addEventListener('mousemove', setIdleTimer);
+  document.addEventListener('keydown', setIdleTimer);
+  loginScreen.addEventListener('mousedown', setIdleTimer);
 
   let idleTimer = null;
   setIdleTimer();
@@ -39,41 +42,43 @@ function start() {
     clearTimeout(idleTimer);
     idleTimer = setTimeout(function() {
       try {
-        timeModal.addEventListener("mousedown", onActive);
-        document.addEventListener("keydown", onActive);
+        timeModal.addEventListener('mousedown', onActive);
+        document.addEventListener('keydown', onActive);
         openTimeModal();
       } catch (e) {
         console.log(e);
-        wrapper.removeEventListener("mousemove", setIdleTimer);
-        document.removeEventListener("keydown", setIdleTimer);
+        wrapper.removeEventListener('mousemove', setIdleTimer);
+        document.removeEventListener('keydown', setIdleTimer);
+        loginScreen.removeEventListener('mousedown', setIdleTimer);
       }
     }, 6000);
   }
-  function onActive({ key }) {
-    if (key && !["Enter", " "].includes(key)) return;
+  function onActive() {
     setIdleTimer();
     closeTimeModal();
-    timeModal.removeEventListener("mousedown", onActive);
-    document.removeEventListener("keydown", onActive);
+    timeModal.removeEventListener('mousedown', onActive);
+    document.removeEventListener('keydown', onActive);
   }
 
   let timeInterval = null;
   setTime();
   timeInterval = setInterval(setTime, 1000);
   function openTimeModal() {
-    timeModal.style.transform = "translateY(0%)";
+    password.blur();
+    timeModal.style.transform = 'translateY(0%)';
   }
   function closeTimeModal() {
-    timeModal.style.transform = "translateY(-100%)";
+    password.focus();
+    timeModal.style.transform = 'translateY(-100%)';
   }
   function setTime() {
     try {
-      const time = timeModal.querySelector(".time");
-      const date = timeModal.querySelector(".date");
+      const time = timeModal.querySelector('.time');
+      const date = timeModal.querySelector('.date');
       const now = new Date();
       time.textContent = formatDate(now);
       date.textContent = `${getDayStr(now.getDay())}, ${getMonthStr(
-        now.getMonth()
+        now.getMonth(),
       )} ${now.getDate()}`;
     } catch (e) {
       console.log(e);
@@ -83,21 +88,21 @@ function start() {
 }
 
 function getDayStr(d) {
-  return "Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday".split(",")[
+  return 'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday'.split(',')[
     d
   ];
 }
 
 function getMonthStr(m) {
-  return "January,February,March,April,May,June,July,August,September,October,November,December".split(
-    ","
+  return 'January,February,March,April,May,June,July,August,September,October,November,December'.split(
+    ',',
   )[m];
 }
 
 function formatDate(d) {
   let h = d.getHours();
   let m = d.getMinutes();
-  h = (h < 10 ? "0" : "") + h;
-  m = (m < 10 ? "0" : "") + m;
+  h = (h < 10 ? '0' : '') + h;
+  m = (m < 10 ? '0' : '') + m;
   return `${h}:${m}`;
 }
