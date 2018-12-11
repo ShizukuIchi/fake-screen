e07d1d1499041974815934b1a2644f446420229b
ShizukuIchi <shizukuichi@gmail.com>
2018-12-11 16:04:13 +0800

    mcdonald

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-import "babel-polyfill";
-import screenfull from "screenfull";
-import "./assets/clear.css";
-import "./assets/font.css";
-import "./style.scss";
+import 'babel-polyfill';
+import screenfull from 'screenfull';
+import './assets/clear.css';
+import './assets/font.css';
+import './style.scss';
 
 const themes = {
-  win10: () => import("./win10/index.js"),
-  "win10-update": () => import("./win10-update/index.js"),
-  "win10-blue": () => import("./win10-blue/index.js"),
-  wannacry: () => import("./wannacry/index.js"),
-  macOS: () => import("./macOS/index.js"),
-  ubuntu1804: () => import("./ubuntu1804/index.js")
+  win10: () => import('./win10/index.js'),
+  'win10-update': () => import('./win10-update/index.js'),
+  'win10-blue': () => import('./win10-blue/index.js'),
+  wannacry: () => import('./wannacry/index.js'),
+  macOS: () => import('./macOS/index.js'),
+  ubuntu1804: () => import('./ubuntu1804/index.js'),
+  mcdonald: () => import('./mcdonald/index.js'),
 };
-const app = document.querySelector("#app");
-const options = document.querySelector(".options");
+const app = document.querySelector('#app');
+const options = document.querySelector('.options');
 
-app.querySelector(".close").onclick = onAppClose;
+app.querySelector('.close').onclick = onAppClose;
 options.onclick = e => {
-  const target = e.target.closest(".option");
+  const target = e.target.closest('.option');
   if (target && target !== options) {
     try {
-      render(e.target.closest(".option").id);
+      render(e.target.closest('.option').id);
     } catch (e) {
       console.log(e);
     }
   } else {
-    console.log("please click on a theme");
+    console.log('please click on a theme');
   }
 };
 
@@ -34,7 +35,7 @@ function render(name) {
   // API can only be initiated by a user gesture.
   renderTheme(name)
     .then(content => {
-      const container = app.querySelector(".content");
+      const container = app.querySelector('.content');
       container.innerHTML = content;
       onAppOpen(name);
     })
@@ -50,13 +51,13 @@ async function renderTheme(name) {
 }
 
 function onAppClose() {
-  history.replaceState(null, "", "./");
+  history.replaceState(null, '', './');
   screenfull.enabled ? screenfull.exit() : undefined;
-  app.style.visibility = "hidden";
+  app.style.visibility = 'hidden';
 }
 function onAppOpen(name) {
   history.pushState({ app: name }, name, name);
-  app.style.visibility = "visible";
+  app.style.visibility = 'visible';
 }
 
 window.onpopstate = e => {
---
+++ src/mcdonald/index.js
@@ -0,0 +1,9 @@
+import mcdonald from './mcdonald.pug';
+import './mcdonald.scss';
+
+export const render = () => {
+  setTimeout(start);
+  return mcdonald;
+};
+
+function start() {}
