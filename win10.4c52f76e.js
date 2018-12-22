1d45e80ea0690b7c8579cbdda8c34fab41e2edcd
ShizukuIchi <shizukuichi@gmail.com>
2018-12-22 19:06:38 +0800

    Updates

--- win10.4c52f76e.js
+++ win10.4c52f76e.js
@@ -239,7 +239,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63703" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44107" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
