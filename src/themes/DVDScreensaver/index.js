46cfd7904111ea7da6f57cd383ac2f49416df461
ShizukuIchi <shizukuichi@gmail.com>
2019-01-26 03:59:34 +0800

    dvd screensaver

--- src/themes/DVDScreensaver/index.js
+++ src/themes/DVDScreensaver/index.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import DVDLogo from './DVDLogo';
 
+const colors = [
+  'white',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'skyblue',
+  'purple',
+];
+
 function DVDScreensaver({ className }) {
-  const [x, setX] = React.useState(10);
-  const [ms, setMs] = React.useState(1000);
-  const [color, setColor] = React.useState('#FFF');
-  function onChange(e) {
-    setX(e.target.value);
+  const [direction, setDirection] = useState({ dx: 1.5, dy: 1.5 });
+  const ref = useRef();
+  const [color, setColor] = useState('#FFF');
+  function change(dx, dy) {
+    setDirection({ dx, dy });
+    setColor(colors[Math.floor(Math.random() * 5)]);
   }
+  function animate() {
+    if (!ref.current) return requestAnimationFrame(animate);
+    let { dx, dy } = direction;
+    const { top, right, bottom, left } = ref.current.getBoundingClientRect();
+    ref.current.style.transform = `translate(${left + dx}px, ${top + dy}px)`;
+    if ((dx < 0 && left <= 0) || (dx > 0 && right >= window.innerWidth))
+      dx = -dx;
+    if ((dy < 0 && top <= 0) || (dy > 0 && bottom >= window.innerHeight))
+      dy = -dy;
+    if (dx !== direction.dx || dy !== direction.dy) change(dx, dy);
+    else requestAnimationFrame(animate);
+  }
+  useEffect(
+    () => {
+      const myRaf = requestAnimationFrame(animate);
+      return () => cancelAnimationFrame(myRaf);
+    },
+    [direction],
+  );
   return (
     <div className={className}>
-      <input type="text" value={x} onChange={onChange} />
       <div
+        ref={ref}
         className="logo"
         style={{
           background: 'black',
-          transform: `translate(${x}px, 0)`,
-          transitionProperty: 'transform',
-          transitionTimingFunction: 'linear',
-          transitionDuration: `${ms}ms`,
         }}
       >
         <DVDLogo color={color} />
@@ -32,8 +59,8 @@ export default styled(DVDScreensaver)`
   background-color: #000;
   height: 100%;
   .logo {
-    width: 200px;
-    height: 88px;
+    width: 300px;
+    height: 132px;
     overflow: hidden;
     background: white;
   }
