617d5971f66b366e57d2ff543dbde74eaeeb8dc1
ShizukuIchi <shizukuichi@gmail.com>
2019-01-31 17:30:30 +0800

    useSafariNoScale hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,4 @@ export {
 } from 'src/hooks/useResettableTimeout';
 export { default as useMediaStyles } from 'src/hooks/useMediaStyles';
 export { default as useMeta } from 'src/hooks/useMeta';
+export { default as useSafariNoScale } from 'src/hooks/useSafariNoScale';
