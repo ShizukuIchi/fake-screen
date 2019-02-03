e80494b8c36f9c23a9d9a47c90b36d3ddd3863c8
ShizukuIchi <shizukuichi@gmail.com>
2019-02-03 23:25:36 +0800

    fix dvd safari bug

--- src/themes/DVDScreensaver/index.js
+++ src/themes/DVDScreensaver/index.js
@@ -1,63 +1,84 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useReducer } from 'react';
 import styled from 'styled-components';
 import { useWindowSize } from 'react-use';
 import { choose } from 'src/lib';
 import DVDLogo from './DVDLogo';
 
-DVDScreensaver.defaultProps = {
-  defaultVelocity: {
-    x: 1.5,
-    y: 1.5,
-  },
-  colors: [
-    'white',
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'skyblue',
-    'purple',
-  ],
+const colors = [
+  'white',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'skyblue',
+  'purple',
+];
+
+export const initState = {
+  color: 'white',
+  velocity: { x: 1.5, y: 1.5 },
 };
 
-function DVDScreensaver({ className, defaultVelocity, colors }) {
+export const reducer = (state = initState, action = {}) => {
+  switch (action.type) {
+    case 'SET':
+      return action.payload;
+    case 'CLEAR':
+      return initState;
+    default:
+      return state;
+  }
+};
+function DVDScreensaver({ className }) {
   const ref = useRef();
   const { width: windowWidth, height: windowHeight } = useWindowSize();
-  const [color, setColor] = useState(choose(colors));
-  const [velocity, setVelocity] = useState(defaultVelocity);
+  const [state, dispatch] = useReducer(reducer, initState);
   useEffect(
     () => {
+      const { velocity } = state;
       let myRaf;
       let canceled = false;
       let vx = velocity.x;
       let vy = velocity.y;
       let {
-        width: logoWidth,
-        height: logoHeight,
         left: logoLeft,
         top: logoTop,
+        width: logoWidth,
+        height: logoHeight,
       } = ref.current.getBoundingClientRect();
       function animate() {
         if (canceled) return;
-        if (
-          (vx < 0 && logoLeft <= 0) ||
-          (vx > 0 && logoLeft + logoWidth >= windowWidth)
-        ) {
-          vx = -vx;
-          setColor(choose(colors));
-          setVelocity({ x: vx, y: vy });
-        } else if (
-          (vy < 0 && logoTop <= 0) ||
-          (vy > 0 && logoTop + logoHeight >= windowHeight)
-        ) {
-          vy = -vy;
-          setColor(choose(colors));
-          setVelocity({ x: vx, y: vy });
+        let shouldUpdate = true;
+        if (logoLeft < 0) {
+          logoLeft = 0;
+          vx = Math.abs(vx);
+        } else if (logoLeft + logoWidth > windowWidth) {
+          logoLeft = windowWidth - logoWidth;
+          vx = -Math.abs(vx);
+        } else if (logoTop < 0) {
+          logoTop = 0;
+          vy = Math.abs(vy);
+        } else if (logoTop + logoHeight > windowHeight) {
+          logoTop = windowHeight - logoHeight;
+          vy = -Math.abs(vy);
         } else {
+          shouldUpdate = false;
           logoLeft += vx;
           logoTop += vy;
           ref.current.style.transform = `translate(${logoLeft}px, ${logoTop}px)`;
+        }
+        if (shouldUpdate) {
+          dispatch({
+            type: 'SET',
+            payload: {
+              velocity: {
+                x: vx,
+                y: vy,
+              },
+              color: choose(colors),
+            },
+          });
         }
         myRaf = requestAnimationFrame(animate);
       }
@@ -72,7 +93,7 @@ function DVDScreensaver({ className, defaultVelocity, colors }) {
   return (
     <div className={className}>
       <div ref={ref} className="logo">
-        <DVDLogo color={color} />
+        <DVDLogo color={state.color} />
       </div>
     </div>
   );
